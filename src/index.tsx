4e108d8f831b4c4eb706b46f6ec16efff44c7d42
Yiwei Gong <imwithye@gmail.com>
2025-06-17 18:08:41 +0800

    hello react

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export const Markdown = () => {
+  return (
+    <View>
+      <Text>Hello, world!</Text>
+    </View>
+  );
+};
+
+export default Markdown;
--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 const banner = `// GitHub: https://github.com/imwithye/react-native-remark`;
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: ["src/index.tsx"],
   splitting: false,
   sourcemap: true,
   clean: true,
