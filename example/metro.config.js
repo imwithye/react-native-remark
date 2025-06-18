963fd257515643333cd692bca256bfa9947bab66
Yiwei Gong <imwithye@gmail.com>
2025-06-18 18:06:12 +0800

    format file

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,19 +1,18 @@
-const path = require('path');
-const { getDefaultConfig } = require('@expo/metro-config');
+/* eslint-disable */
+const { resolve } = require("path");
+const { getDefaultConfig } = require("@expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
-config.watchFolders = [
-  path.resolve(__dirname, '..', 'src'),
-];
+config.watchFolders = [resolve(__dirname, "..", "src")];
 
 config.resolver.extraNodeModules = {
-  'react-native-remark': path.resolve(__dirname, '..', 'src'),
+  "react-native-remark": resolve(__dirname, "..", "src"),
 };
 
 config.resolver.nodeModulesPaths = [
-  path.resolve(__dirname, 'node_modules'),
-  path.resolve(__dirname, '..', 'node_modules'),
+  resolve(__dirname, "node_modules"),
+  resolve(__dirname, "..", "node_modules"),
 ];
 
 module.exports = config;
