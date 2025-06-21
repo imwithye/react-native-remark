357b3810d26e67f7136a1d9fda22d2346b7a2682
Yiwei Gong <imwithye@gmail.com>
2025-06-21 15:43:15 +0800

    render image with width and height

--- src/renderers/image.tsx
+++ src/renderers/image.tsx
@@ -1,6 +1,6 @@
 import { ImageReference, Image as MdImage } from "mdast";
-import { ReactNode } from "react";
-import { Image } from "react-native";
+import { ReactNode, useState } from "react";
+import { Dimensions, Image } from "react-native";
 
 import { useMarkdownContext } from "../context";
 import { RendererArgs } from "./renderers";
@@ -18,5 +18,21 @@ export const ImageReferenceRenderer = ({
 };
 
 export const ImageRenderer = ({ node }: RendererArgs<MdImage>): ReactNode => {
-  return <Image source={{ uri: node.url }} style={{width: 100, height: 100}} />;
+  const [width, setWidth] = useState<number | null>(0);
+  const [height, setHeight] = useState<number | null>(0);
+
+  return (
+    <Image
+      onLoad={(e) => {
+        const { width, height } = e.nativeEvent.source;
+        const ratio = height / width;
+        const contentWidth = Dimensions.get("window").width;
+        const contentHeight = contentWidth * ratio;
+        setWidth(contentWidth);
+        setHeight(contentHeight);
+      }}
+      source={{ uri: node.url }}
+      style={{ width: width, height: height, borderRadius: 5 }}
+    />
+  );
 };
